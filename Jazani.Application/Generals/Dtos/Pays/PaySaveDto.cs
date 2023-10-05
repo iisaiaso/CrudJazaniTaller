909c38bba9d77b576b82340b2f77e6c77c72aebc
iisaiaso <isaiasibarraonofre@gmail.com>
2023-10-05 01:48:03 -0500

    Adding table implementation pay and project completed

--- Jazani.Application/Generals/Dtos/Pays/PaySaveDto.cs
+++ Jazani.Application/Generals/Dtos/Pays/PaySaveDto.cs
@@ -8,9 +8,18 @@ namespace Jazani.Application.Generals.Dtos.Pays
 {
     public class PaySaveDto
     {
+        
         public int Year { get; set; }
         public string? ReceiptNumber { get; set; }
         public decimal IndividualCost { get; set; }
+        public decimal AmountPaid { get; set; }
+        public int PeriodTypeId { get; set; }
         public int FinancialentityId { get; set; }
+        public int CurrencyTypeId { get; set; }
+        public int MiningConcessionId { get; set; }
+        public int ConceptId { get; set; }
+        public string? Description { get; set; }
+        public decimal AvailableArea { get; set; }
+         
     }
 }
